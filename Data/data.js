a85e1bef0ead3858c99ae472c2619efd7f0ee84a
Muhamad Rafiki <rafikimuhammad@gmail.com>
2023-11-03 11:40:29 -0500

    first commit

---
+++ Data/data.js
@@ -0,0 +1,18 @@
+const data = [
+    {
+        name: "casino",
+        url: "casino.png"
+    },
+    {
+        name:"casino1",
+        url:"casino1.png"
+    },
+    {
+        name:"casino2",
+        url:"casino2.png"
+    },
+    {
+        name:"casino3",
+        url:"casino3.png"
+    }
+];
---
+++ scripts/photo.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded",()=>{
+
+const photoList = document.getElementById("photoList");
+const photo = document.getElementById("photo");
+
+for(let i = 0; i <data.length ; i++){
+photoList.innerHTML += `<option value= " ${data[i].url}"> ${data[i].name}</option>`;
+}
+photoList.addEventListener("change",(e)=>{
+    photo.src ="./Data/images" + e.target.value
+});
+
+})
